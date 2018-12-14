b70705cf7a1cc77bea4060f207286011efd3a8bf
Zlira <olesia.hr@gmail.com>
2018-12-14 20:22:37 +0200

    Kinda working inserts in formula editor

--- src/components/FormulaEditor.js
+++ src/components/FormulaEditor.js
@@ -1,20 +1,73 @@
 import React from 'react'
+import {bisectLeft} from 'd3-array'
 
-function OperatorButton({operator, handleClick}) {
-  return <button onClick={handleClick}>{operator}</button>
+
+class formulaHandler {
+  // convert tokens to representation (without sqr and sqrt for now)
+  // move cursor so it treats var name as one element
+  // add new tokens given cursor position (add spaces, return new cursor position)
+  // remove tokens given selection bounds, direction (return new cursor position)
+  // replace tokens givent selection bounds, new token (retrun new cursor position)
+  constructor() {
+    this.tokens = []
+  }
+
+  _getTokenIdeces() {
+    let count = 0
+    const indeces = []
+    for (const token of this.tokens) {
+      if (indeces.length) {
+        count++
+      }
+      indeces.push({
+        start: count,
+        end: count += token.symbol.length
+      })
+    }
+    return indeces
+  }
+
+  addToken(token, cursorPosition) {
+    let insertIndex
+    const newToken = {symbol: token}
+    const tokenIndeces = this._getTokenIdeces()
+    if (cursorPosition.start === cursorPosition.end) {
+      insertIndex = bisectLeft(
+        tokenIndeces.map(e => e.start),
+        cursorPosition.start
+      )
+    } else {
+      insertIndex = this.tokens.length
+    }
+    // debugger
+    this.tokens.splice(insertIndex, 0, newToken)
+    return (
+      (tokenIndeces[insertIndex - 1]? tokenIndeces[insertIndex - 1].end : 0) +
+      token.length +
+      (insertIndex? 1 : 0))
+    }
+
+  repr() {
+    return this.tokens.map(el => el.symbol).join(' ')
+  }
 }
 
+function InsertTokenButton({token, handleClick}) {
+  return <button onClick={handleClick}>{token}</button>
+}
 
 function EditorControls({addToken}) {
   const vars = ['AB', 'BC']
   const operators = ['+', '-', '×', '÷', '^2', '√']
   const opButtons = operators.map(
-    el => <OperatorButton
-             key={el} operator={el}
+    el => <InsertTokenButton
+             key={el} token={el}
              handleClick={() => addToken(el)}/>
   )
   const varButtons = vars.map(
-    el => <button key={el}>{el}</button>
+    el => <InsertTokenButton
+             key={el} token={el}
+             handleClick={() => addToken(el)}/>
   )
   return (
     <div className="editorControls">
@@ -31,22 +84,44 @@ export default class FormulaEditor extends React.Component {
     this.state = {
       formula: ''
     }
+    this.formulaHandler = new formulaHandler()
     this.inputRef = React.createRef()
 
     this.addToken = this.addToken.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
 
+  componentDidUpdate() {
+    if (this.nextCursorPos) {
+      this.inputRef.current.setSelectionRange(
+        this.nextCursorPos, this.nextCursorPos
+      )
+    }
+    this.nextCursorPos = undefined
+  }
+
   componentDidMount() {
-    this.inputRef.current.addEventListener('keypress', this.handleKeydown, false)
+    this.inputRef.current.addEventListener(
+      'keypress', this.handleKeydown, false
+    )
+  }
+
+  componentWillUnmount() {
+    this.inputRef.current.removeEventListener(
+      'keypress', this.handleKeydown
+    )
   }
 
   addToken(token) {
-    this.setState(
-      prevState => ({
-          formula: prevState.formula + token,
-      })
-    )
+    const selection = {
+      start: this.inputRef.current.selectionStart,
+      end: this.inputRef.current.selectionEnd,
+    }
+    this.nextCursorPos = this.formulaHandler.addToken(token, selection)
+    this.setState({
+      formula: this.formulaHandler.repr(),
+    })
+    this.inputRef.current.focus()
   }
 
   handleKeydown(keyDownEvent) {
